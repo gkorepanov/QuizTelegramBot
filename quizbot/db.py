9988fe4aeaa5a37fa0218f276ab0ff649ac383d1
George Korepanov <gkorepanov.gk@gmail.com>
2019-10-14 02:19:18 +0300

    Use single mongoDB HOST env variable instead of 2

--- quizbot/db.py
+++ quizbot/db.py
@@ -1,5 +1,5 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.environ['MONGO_HOST'], int(os.environ['MONGO_PORT']))
+client = MongoClient(os.environ['MONGO_HOST'])
 db = client['quiz_posts']
